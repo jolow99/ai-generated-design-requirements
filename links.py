80f343dc04630fd6fc85168b69b0b142a7cd9ce8
Jolow <jolow999@gmail.com>
2022-04-11 19:13:49 +0800

    Updated scripts for edge case handling

---
+++ links.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd 
+
+
+url = "https://www.delonghi.com/en/products/coffee/c/coffee?q=%3Arelevance%3Aseries%3AMagnifica%2BEvo%3Aseries%3AVertuoNext%3Aseries%3ADedica%3Aseries%3AManual%2Bespresso%2Bmakers%3Aseries%3AFilter%2Bcoffee%2Bmakers%3Aseries%3AMagnifica%2BS%3Aseries%3AAutomatic%2Bcoffee%2Bmakers%3Aseries%3ADinamica%2BPlus%3Aseries%3AIcona%2BVintage%3Aseries%3AMagnifica%3Aseries%3APrimaDonna%2BSoul%3Aseries%3ACombi%2Bcoffee%2Bmakers%3Aseries%3ADinamica%3Aseries%3AMagnifica%2BS%2BSmart%3Aseries%3AGran%2BLattissima%3Aseries%3AIcona%2B%2526%2BDedica%2BMetallics%3Aseries%3ALa%2BSpecialista%3Aseries%3ALattissima%2BOne%3Aseries%3APerfecta%2BEvo%3Aseries%3APrimaDonna%2BElite%3Aseries%3APerfecta%2BDeluxe%3Aseries%3APrimaDonna%2BClass%3Aseries%3AActive%2BLine%3Aseries%3AEletta%3Aseries%3AEletta%2BCappuccino%2BEvo%3Aseries%3ALattissima%2BPro"
+
+# Using requests
+# r = requests.get(url)
+# soup = BeautifulSoup(r.content, 'html5lib')
+
+# Using Selenium
+data = open('DS.html','r', encoding ="utf-8")
+soup = BeautifulSoup(data, 'html5lib')
+
+table = soup.find_all('a', attrs={'class': 'js-gtm-product-click'})
+
+links = []
+stoplinks = ["https://www.delonghi.com/en/kg521-m-dedica-electric-coffee-grinder/p/KG521.M"]
+
+for row in table:
+    link = "https://www.delonghi.com" + row.get('href')
+    if link not in stoplinks and link not in links:
+        links.append(link)
+
+# Write to CSV file by creating a dataframe
+df = pd.DataFrame({"links": links})
+df.to_csv("links.csv", index=False)
--- main.py
+++ main.py
@@ -4,22 +4,42 @@ import csv
 import os 
 import pandas as pd 
  
-# reading CSV file as dataframe and onvert to list
+# reading CSV file as dataframe and convert to list
 df = pd.read_csv("links.csv")
 links = df['links'].tolist()
+stoplinks = ["https://www.delonghi.com/en/ec9355-bm-la-specialista-prestigio-manual-espresso-maker/p/EC9355.BM"]
 
 for url in links: 
     print("Current URL:", url)
     model = url.split('/')[-1]
     r = requests.get(url)
-
     soup = BeautifulSoup(r.content, 'html5lib')
     data, labels, values = {"model": model}, [], []
-    table = soup.find('div', attrs={'class': 'del-simple-css-accordion__content-desktop'})
+    
+    if url not in stoplinks: 
+        table = soup.find('div', attrs={'class': 'del-simple-css-accordion__content-desktop'})
+    else: 
+        # hot-fix for one product
+        table = soup.find_all('div', attrs={'class': 'del-simple-css-accordion__content-desktop'})[1]
+
     for label in table.findAll('span', attrs = {'class':"del-pdp__specifications__single__label"}):
+        if label.string == "Dimensions (wxdxh-mm) without filter holder" or label.string == "Dimensions (wxdxh)": 
+            label.string = "Dimensions (wxdxh) (mm)"
+        if label.string == "Pump pressure":
+            label.string = "Pump pressure (bar)"
+        if label.string == "Max cup height (mm)":
+            label.string = "Maximum cup height (mm)"
         labels.append(label.string)
     for value in table.findAll('span', attrs = {'class':"del-pdp__specifications__single__value"}):
-        values.append(value.string.strip())
+        if value.string != None: 
+            print("Value:",value)
+            values.append(value.string.strip())
+        else: 
+            print("Value:",value)
+            energy_class = value.i["class"][-1]
+            energy_class_label = energy_class[-1]
+            print(energy_class_label)
+            values.append(energy_class_label)
     for i in range(len(labels)):
         data[labels[i]] = values[i]
 
@@ -32,6 +52,6 @@ for url in links:
         w = csv.DictWriter(f,data.keys())
         w.writeheader()
         w.writerow(data)
-        
-    print("Done with", model)
-    print("")
+            
+        print("Done with", model)
+        print("")
